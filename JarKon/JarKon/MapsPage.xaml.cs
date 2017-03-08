1d3332dc21124d95b59f917cea58c7f46d4eec35
Metzing Márton <marton.metzing@gmail.com>
2017-03-08 17:44:31 +0100

    Added tabs

--- JarKon/JarKon/MapsPage.xaml.cs
+++ JarKon/JarKon/MapsPage.xaml.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace JarKon
 {
-	public partial class MainPage : TabbedPage
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MapsPage : ContentPage
 	{
-		public MainPage()
+		public MapsPage ()
 		{
-			InitializeComponent();
+			InitializeComponent ();
 		}
 	}
 }

d30ad4466a71a868b53a70082d52273425ef1b70
Metzing Márton <marton.metzing@gmail.com>
2017-04-18 13:09:26 +0200

    Repair

---
+++ JarKon/JarKon/View/CardsPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using JarKon.Core;
+using JarKon.ViewModel;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Collections.Specialized;
+
+namespace JarKon.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CardsPage : ContentPage
+    {
+        public CardsPage()
+        {
+            InitializeComponent();
+            Provider.Instance.CardsPage = this;
+        }
+    }
+}

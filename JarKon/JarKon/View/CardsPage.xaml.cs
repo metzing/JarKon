8ffcfbee4d5abe658caabb6a2b2655eebf4c7b87
Metzing Márton <marton.metzing@gmail.com>
2017-04-19 13:01:15 +0200

    repair

---
+++ JarKon/JarKon/View/CardsPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using JarKon.Core;
+using JarKon.View.CardDetails;
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace JarKon.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CardsPage : ContentPage
+    {
+        public CardsPage()
+        {
+            InitializeComponent();
+            Provider.Instance.CardsPage = this;
+        }
+
+        public void LoadCards()
+        {
+            Device.BeginInvokeOnMainThread(
+                () =>
+                {
+                    CardList.BuildCards();
+                });
+        }
+    }
+}
